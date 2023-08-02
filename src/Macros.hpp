8ad4c6bf96ec16580b5fbde575a23235be1ad8cc
Alphacharge <rbetz@student.42heilbronn.de>
2023-08-02 16:30:17 +0000

    removed unused definition

--- src/Macros.hpp
+++ src/Macros.hpp
@@ -6,12 +6,10 @@
 /*   By: rbetz <rbetz@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 13:57:29 by lsordo            #+#    #+#             */
-/*   Updated: 2023/08/02 14:28:36 by rbetz            ###   ########.fr       */
+/*   Updated: 2023/08/02 16:28:01 by rbetz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 # define VERBOSE	3 //0 zero output, 1 errors, 2 warnings, 3 debug
-
-# define ERR_EMPTY_STACK		"Stack is empty !"
